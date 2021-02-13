03328ec4e6a35c97f4412e688900a67639e1778d
Jayden <jayden@jaydensar.net>
2021-02-12 23:48:36 -0500

    switch to pnpm, add eslint and prettier config, format files

--- src/connection.js
+++ src/connection.js
@@ -1,9 +1,9 @@
 const REQUEST_VALITDITY_TIME = 55 * 1000; // 60s for game servers, none for studio (always assumes gameserver)
 
-const EventEmitter = require("events");
+const EventEmitter = require('events');
 
-const { performance } = require("perf_hooks");
-const uuid = require("uuid");
+const { performance } = require('perf_hooks');
+const { v4: uuid } = require('uuid');
 
 class Connection extends EventEmitter {
 	constructor() {
@@ -21,7 +21,12 @@ class Connection extends EventEmitter {
 	// Recieve logic
 
 	_emptyQueue(force) {
-		if (force || this._req && this._queue.length > 0 && performance.now() - this._last <= REQUEST_VALITDITY_TIME) {
+		if (
+			force ||
+			(this._req &&
+				this._queue.length > 0 &&
+				performance.now() - this._last <= REQUEST_VALITDITY_TIME)
+		) {
 			this._res.status(200).send(this._queue);
 
 			this._req = null;
--- src/server.js
+++ src/server.js
@@ -1,94 +1,96 @@
-const EventEmitter = require("events");
-const Connection = require("./connection");
+const EventEmitter = require('events');
+const Connection = require('./connection');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 class rbxwebhookserver extends EventEmitter {
-  constructor(options) {
-    super();
+	constructor(options) {
+		super();
 
-    this.connections = {};
-    this.options = options;
+		this.connections = {};
+		this.options = options;
 
-    var server = express.Router();
+		const server = express.Router();
 
-    function checkAuthentication(req, res, next) {
-      if (options !== undefined) {
-        // There are avaliable options to read through.
-        if (options["apiKey"]) {
-          // checks if apiKey exists in the options.
-          var setKey = options["apiKey"];
-          var checkedKey = req.headers.authorization;
-          if (setKey === checkedKey) {
-            // Authentication was a success and can continue to connect.
-            next();
-          } else {
-            // Authentication failed.
-            res.status(401).json({ error: "Authentication failed." });
-          }
-        } else {
-          // if it doesn't we're going to authenticate because there are no protections
-          next();
-        }
-      } else {
-        // There are no options so we are just going to authenticate any request.
-        next();
-      }
-    }
+		const checkAuthentication = (req, res, next) => {
+			if (options !== undefined) {
+				// There are avaliable options to read through.
+				if (options['apiKey']) {
+					// checks if apiKey exists in the options.
+					const setKey = options['apiKey'];
+					const checkedKey = req.headers.authorization;
+					if (setKey === checkedKey) {
+						// Authentication was a success and can continue to connect.
+						next();
+					} else {
+						// Authentication failed.
+						res.status(401).json({
+							error: 'Authentication failed.',
+						});
+					}
+				} else {
+					// if it doesn't we're going to authenticate because there are no protections
+					next();
+				}
+			} else {
+				// There are no options so we are just going to authenticate any request.
+				next();
+			}
+		};
 
-    // Connect
-    server.get("/connect", checkAuthentication, (req, res) => {
-      var connection = new Connection();
-      this.connections[connection.id] = connection;
-      this.emit("connection", connection);
+		// Connect
+		server.get('/connect', checkAuthentication, (req, res) => {
+			const connection = new Connection();
+			this.connections[connection.id] = connection;
+			this.emit('connection', connection);
 
-      res.status(201).json({ id: connection.id });
-    });
+			res.status(201).json({ id: connection.id });
+		});
 
-    server.use((req, res, next) => {
-      // Authentication middleware
-      var connection = this.connections[req.headers["connection-id"]]; // finds connection with cooresponding ID
+		server.use((req, res, next) => {
+			// Authentication middleware
+			const connection = this.connections[req.headers['connection-id']]; // finds connection with cooresponding ID
 
-      if (connection) {
-        req.connection = connection;
-        next();
-      } else {
-        res.status(401).json({ error: "Not connected" });
-      }
-    });
+			if (connection) {
+				req.socket = connection;
+				next();
+			} else {
+				res.status(401).json({ error: 'Not connected' });
+			}
+		});
 
-    server.get("/disconnect", (req, res) => {
-      req.connection.emit("disconnect");
+		server.get('/disconnect', (req, res) => {
+			req.socket.emit('disconnect');
 
-      delete this.connections[req.connection.id];
-      res.status(200).send("ok");
-    });
+			delete this.connections[req.socket.id];
+			res.status(200).send('ok');
+		});
 
-    // Get/recieve
+		// Get/recieve
 
-    server.get("/data", (req, res) => {
-      req.connection.onRequest(req, res);
-    });
+		server.get('/data', (req, res) => {
+			req.socket.onRequest(req, res);
+		});
 
-    server.post("/data", bodyParser.json(), (req, res) => {
-      if (req.body.t) {
-        req.connection.emit(req.body.t, req.body.d);
-        res.status(200).send("ok");
-      } else {
-        res.status(400).send({ error: "Invalid target" });
-      }
-    });
+		server.post('/data', bodyParser.json(), (req, res) => {
+			if (req.body.t) {
+				req.socket.emit(req.body.t, req.body.d);
+				res.status(200).send('ok');
+			} else {
+				res.status(400).send({ error: 'Invalid target' });
+			}
+		});
 
-    this.router = server;
-  }
+		this.router = server;
+	}
 
-  broadcast(target, data) {
-    // broadcasts the event to every avaliable
-    Object.keys(this.connections).forEach(id => {
-      this.connections[id].send(target, data);
-    });
-  }
+	broadcast(target, data) {
+		// broadcasts the event to every avaliable
+		Object.keys(this.connections).forEach((id) => {
+			this.connections[id].send(target, data);
+		});
+	}
 }
 
 module.exports = rbxwebhookserver;
